3d339fe5ccd30e01489f848d9773bbd015a65e4f
Eng-Mosad-99 <mohammedmosaad869@gmail.com>
2026-04-23 23:25:54 +0200

    add  keys in ar.json & en.json

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,40 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:localization_app/screens/home_screen.dart';
 
-void main() {
-  runApp(const LocalizationApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+
+  runApp(
+    EasyLocalization(
+       supportedLocales: const [
+        Locale('en'),
+        Locale('ar'),
+      ],
+      path: 'assets/translations',
+      startLocale: const Locale('en'),
+      fallbackLocale: const Locale('en'),
+      saveLocale: true,
+      child: LocalizationApp(),
+    ),
+  );
 }
 
 class LocalizationApp extends StatelessWidget {
   const LocalizationApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'Localization App',
       theme: ThemeData(
-       
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
       home: HomeScreen(),
     );
   }
