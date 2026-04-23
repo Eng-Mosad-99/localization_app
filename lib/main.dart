32f10411aecc3bacd6637b10cc1e603f30b65ac0
Eng-Mosad-99 <mohammedmosaad869@gmail.com>
2026-04-23 23:31:41 +0200

    install package easy logger to hide Easy Localization logger

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:easy_localization/easy_localization.dart';
+import 'package:easy_logger/easy_logger.dart';
 import 'package:flutter/material.dart';
 import 'package:localization_app/screens/home_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
+  EasyLocalization.logger.enableLevels = <LevelMessages>[
+    LevelMessages.warning,
+  ];
 
   runApp(
     EasyLocalization(
